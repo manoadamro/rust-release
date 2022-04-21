3c8b0fdf7ef387a06e8471b572cd7a168942e00f
Adam Romano <manoadamro.imp@gmail.com>
2022-04-21 19:06:19 +0100

    testing

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const core = require('@actions/core');
 const glob = require('@actions/glob');
-const { promises: fs } = require('fs')
+const { Octokit } = require("@octokit/rest");
+const fs = require('fs')
 
-async function get_cargo_content(cargo_path) {
-  await fs.access(cargo_path, async error => {
-    if (error) {
-      throw new Error(`Action failed to read file ${cargo_path} (${error})`)
-    } else {
-      await fs.readFile(cargo_path, 'utf8')
-    }
-  })
-}
-
-async function get_crate_version(cargo_content) {
-  let cargo_version_re = /version *= *"(?<version>[a-zA-Z0-9._-]+)"/u;
-  let cargo_version_result = cargo_version_re.exec(cargo_content);
-  let cargo_version = cargo_version_result.groups.version;
-  if (!cargo_version) {
-    throw new Error("Action failed to find version in crate.")
-  }
-}
 
 // most @actions toolkit packages have async methods
 async function run() {
+  core.info("Starting...")
+
   try {
     // get workflow inputs
+    const owner = core.getInput(`owner`, { required: true });
+    const repo = core.getInput(`repo`, { required: true });
     const token = core.getInput('token', { required: true });
-    const branch = core.getInput('branch', { required: true });
-    const cargo_path = core.getInput('cargo', { required: true })
+    const cargo_path = core.getInput('cargo', { required: true });
 
-    const octokit = github.getOctokit(token)
+    core.info("Getting cargo file contents...");
+    const cargo_content = fs.readFileSync(cargo_path, 'utf8').toString();
 
-    // get crate version from Cargo.toml
-    const version = get_cargo_content(cargo_path).then(get_crate_version)
+    core.info("Getting crate version...");
+    let cargo_version_re = /version *= *"(?<version>[a-zA-Z0-9._-]+)"/u;
+    let cargo_version_result = cargo_version_re.exec(cargo_content);
+    let cargo_version = cargo_version_result.groups.version;
+    core.info(`Got crate version ${cargo_version}`);
 
     // check current releases for existing version
-    const releases = await octokit.rest.release.get({
-      owner: 'octokit',
-      repo: 'rest.js',
-      pull_number: 123,
-      mediaType: {
-        format: 'diff'
-      }
+    const release_name = `v${cargo_version}`
+    const octokit = github.getOctokit(token);
+    const releases = await octokit.rest.repos.listReleases({
+      owner: owner,
+      repo: repo,
     });
-    console.log(releases);
+    const existing = releases.data.some(i => i.name === release_name);
+    if (existing) {
+      core.info(`Skipping: Release with tag ${cargo_version} already exists`)
+      core.notice(`Release with tag ${cargo_version} already exists`)
+    } else {
+      core.info(`Creating release with tag ${cargo_version}...`)
+      const _response = octokit.rest.repos.createRelease({
+        owner: owner,
+        repo: repo,
+        tag_name: release_name,
+        name: release_name,
 
-    // create a new release
+      })
+      core.notice(`Created release with tag ${cargo_version}`)
+    }
 
     // output the crate version
-    core.setOutput('version', version);
+    core.setOutput('version', cargo_version);
   } catch (error) {
     core.setFailed(error.message);
   }
